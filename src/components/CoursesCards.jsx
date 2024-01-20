64828fb588508425a3452aa817eae5772b89427a
Tornikesamkharadze <tornikesmakharadzee@gmail.com>
2024-01-20 17:01:35 +0400

    created data for my coursecards component

--- src/components/CoursesCards.jsx
+++ src/components/CoursesCards.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import CoursesData from "../data/CoursesData";
 
 const CoursesCards = () => {
+    console.log(CoursesData[0].imageUrl)
   return (
     <CardsWrapper>
       <CardWrapper>
         <h6 className="heading6">სასწავლო კურსები</h6>
+        <img src={CoursesData[0].imageUrl} alt="" />
+        <h1>{CoursesData[0].title}</h1>
       </CardWrapper>
     </CardsWrapper>
   );
@@ -23,7 +27,7 @@ const CardWrapper = styled.div`
   padding: 0px 20px;
   .heading6 {
     font-size: 24px;
-    color: rgb(219,219,219);
+    color: rgb(219, 219, 219);
     font-weight: 300;
     padding: 50px 0px 80px 0px;
   }
---
+++ src/data/CoursesData.jsx
@@ -0,0 +1,75 @@
+import AdvancedCybersecurityCourse from "../assets/images/coursesImages/AdvancedCybersecurityCourse.webp";
+import AdvancedPython from "../assets/images/coursesImages/AdvancedPython.webp";
+import Blockchain from "../assets/images/coursesImages/Blockchain.webp";
+import DevOps from "../assets/images/coursesImages/DevOps.webp";
+import InformationSecurityGovernance from "../assets/images/coursesImages/InformationSecurityGovernance.webp";
+import IntrotoPython from "../assets/images/coursesImages/IntrotoPython.webp";
+import iOSDevelopment from "../assets/images/coursesImages/iOSDevelopment.webp";
+import React from "../assets/images/coursesImages/React.webp";
+import ToTTrainingofTrainers from "../assets/images/coursesImages/ToTTrainingofTrainers.webp";
+
+export default [
+  {
+    id: 1,
+    imageUrl: iOSDevelopment,
+    courseTitle: "iOS Development",
+    courseStatus: "რეგისტრაცია დასრულებულია",
+    courseDetailsLink: "#",
+  },
+  {
+    id: 2,
+    imageUrl: React,
+    courseTitle: "React",
+    courseStatus: "რეგისტრაცია დასრულებულია",
+    courseDetailsLink: "#",
+  },
+  {
+    id: 3,
+    imageUrl: IntrotoPython,
+    courseTitle: "Intro to Python",
+    courseStatus: "რეგისტრაცია დასრულებულია",
+    courseDetailsLink: "#",
+  },
+  {
+    id: 4,
+    imageUrl: AdvancedPython,
+    courseTitle: "Advanced Python",
+    courseStatus: "რეგისტრაცია დასრულებულია",
+    courseDetailsLink: "#",
+  },
+  {
+    id: 5,
+    imageUrl: AdvancedCybersecurityCourse,
+    courseTitle: "Advanced Cybersecurity Course",
+    courseStatus: "რეგისტრაცია დასრულებულია",
+    courseDetailsLink: "#",
+  },
+  {
+    id: 6,
+    imageUrl: ToTTrainingofTrainers,
+    courseTitle: "ToT - Training of Trainers",
+    courseStatus: "რეგისტრაცია დასრულებულია",
+    courseDetailsLink: "#",
+  },
+  {
+    id: 7,
+    imageUrl: Blockchain,
+    courseTitle: "Blockchain",
+    courseStatus: "რეგისტრაცია დასრულებულია",
+    courseDetailsLink: "#",
+  },
+  {
+    id: 8,
+    imageUrl: DevOps,
+    courseTitle: "DevOps",
+    courseStatus: "რეგისტრაცია დასრულებულია",
+    courseDetailsLink: "#",
+  },
+  {
+    id: 9,
+    imageUrl: InformationSecurityGovernance,
+    courseTitle: "Information Security Governance",
+    courseStatus: "რეგისტრაცია დასრულებულია",
+    courseDetailsLink: "#",
+  },
+];
