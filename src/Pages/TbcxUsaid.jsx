97a59e6af11f810f1a008886066fd4e20a27da59
Tornikesamkharadze <tornikesmakharadzee@gmail.com>
2024-01-21 19:05:06 +0400

    imported new component

--- src/Pages/TbcxUsaid.jsx
+++ src/Pages/TbcxUsaid.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import TbcTeam from "../components/TbcTeam";
 import TbcInfo from "../components/TbcInfo";
 import CoursesCards from "../components/CoursesCards";
-
+import TbcQnA from "../components/TbcQnA";
 const TbcxUsaid = () => {
   return (
     <>
       <TbcTeam />
       <TbcInfo />
       <CoursesCards />
+      <TbcQnA />
     </>
   );
 };
