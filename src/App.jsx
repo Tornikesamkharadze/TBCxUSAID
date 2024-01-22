32bdbcdfc2ebef9257b0a3c1f0b71ac2ca108e2c
Tornikesamkharadze <tornikesmakharadzee@gmail.com>
2024-01-22 21:42:57 +0400

    testing something

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+// App.jsx
+
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import routes from "./routes";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import MobileFooter from "./components/MobileFooter";
 
 const App = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <BrowserRouter>
@@ -19,7 +33,7 @@ const App = () => {
             />
           ))}
         </Routes>
-        <Footer />
+        {isMobile ? <MobileFooter /> : <Footer />}
       </BrowserRouter>
     </>
   );
--- src/components/MobileFooter.jsx
+++ src/components/MobileFooter.jsx
@@ -4,7 +4,7 @@ import footerLogo from "../assets/images/footer/footerTbcLogo.webp";
 import tbcFbIcon from "../assets/images/footer/tbcFb.webp";
 import tbcYtIcon from "../assets/images/footer/tbcYt.webp";
 
-const Footer = () => {
+const MobileFooter = () => {
   return (
     <StyledFooter>
       <FooterContent>
@@ -12,32 +12,32 @@ const Footer = () => {
           <FooterLogo>
             <img src={footerLogo} alt="Logo" />
           </FooterLogo>
+          <FooterSocialMedia>
+            <MailWrapper>
+              <a href="mailto:your@email.com">მოგვწერეთ</a>
+            </MailWrapper>
+            <SocialWrapper>
+              <a
+                href="https://www.facebook.com/tbcbank/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={tbcFbIcon} alt="Facebook" />
+              </a>
+              <a
+                href="https://www.youtube.com/channel/UCGk9R2LV2ywOL80r8Xc6xtQ"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={tbcYtIcon} alt="Youtube" />
+              </a>
+            </SocialWrapper>
+          </FooterSocialMedia>
           <FooterBottom>
-            <p>&copy; 2023 ყველა უფლება დაცულია</p>
             <button className="conditionsBtn">წესები და პირობები</button>
+            <p>&copy; 2023 ყველა უფლება დაცულია</p>
           </FooterBottom>
         </div>
-        <FooterSocialMedia>
-          <MailWrapper>
-            <a href="mailto:your@email.com">მოგვწერეთ</a>
-          </MailWrapper>
-          <SocialWrapper>
-            <a
-              href="https://www.facebook.com/tbcbank/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={tbcFbIcon} alt="Facebook" />
-            </a>
-            <a
-              href="https://www.youtube.com/channel/UCGk9R2LV2ywOL80r8Xc6xtQ"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={tbcYtIcon} alt="Youtube" />
-            </a>
-          </SocialWrapper>
-        </FooterSocialMedia>
       </FooterContent>
     </StyledFooter>
   );
@@ -53,48 +53,41 @@ const StyledFooter = styled.footer`
 `;
 
 const FooterContent = styled.div`
-  display: flex;
-  justify-content: space-between;
   max-width: 1000px;
   width: 100%;
   padding: 0px 20px 0px 20px;
-  .wrapColum {
-    display: flex;
-    flex-direction: column;
-  }
 `;
 
 const FooterLogo = styled.div``;
 
 const FooterSocialMedia = styled.div`
   display: flex;
-  flex-direction: column;
-  gap: 60px;
+  max-width: 700px;
+  width: 100%;
+
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 30px 0px;
 `;
+
 const MailWrapper = styled.div`
-  order: 3;
   a {
+    display: inline-block;
     padding: 10px 40px;
     background-color: rgb(0, 174, 243);
     border-radius: 8px;
     font-size: 20px;
     font-family: 300;
     color: white;
-    transition: background-color 0.3s, color 0.3s;
-    &:hover {
-      color: black;
-      background-color: rgb(148, 148, 148);
-    }
   }
 `;
 
 const SocialWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  a:nth-child(2) {
-    margin-left: 20px;
+  a {
+    margin-left: 30px;
   }
 `;
+
 const FooterBottom = styled.div`
   .conditionsBtn {
     font-size: 16px;
@@ -102,14 +95,10 @@ const FooterBottom = styled.div`
     background-color: transparent;
     border: none;
     cursor: pointer;
-    transition: 0.9s ease;
-    &:hover {
-      color: gray;
-    }
   }
   p {
     margin: 30px 0px;
   }
 `;
 
-export default Footer;
+export default MobileFooter;
