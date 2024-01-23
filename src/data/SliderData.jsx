2c66d23a980c18be031d7a18549549cc1e1fc1bf
Tornikesamkharadze <tornikesmakharadzee@gmail.com>
2024-01-23 19:51:08 +0400

    created data for partner project slider

---
+++ src/data/SliderData.jsx
@@ -0,0 +1,23 @@
+import usaid from "../assets/images/projectPartners/usaid.webp";
+import spaceInt from "../assets/images/projectPartners/spaceInt.webp";
+import tnt from "../assets/images/projectPartners/tnt.webp";
+import tegeta from "../assets/images/projectPartners/tegeta.webp";
+import spectre from "../assets/images/projectPartners/spectre.webp";
+import tbc from "../assets/images/projectPartners/tbc.webp";
+import ufc from "../assets/images/projectPartners/ufc.webp";
+
+export default [
+  {
+    id: 0,
+    imageUrl: [usaid, spaceInt, tnt],
+  },
+  {
+    id: 1,
+    imageUrl: [tegeta, spectre, tbc],
+  },
+
+  {
+    id: 2,
+    imageUrl: [ufc],
+  },
+];
