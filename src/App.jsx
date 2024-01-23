654dbbffdc61fb497333bd1be64b9b9f9828428e
Tornikesamkharadze <tornikesmakharadzee@gmail.com>
2024-01-23 13:05:01 +0400

    Removed unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-// App.jsx
-
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import routes from "./routes";
 import Navbar from "./components/Navbar";
@@ -8,17 +6,6 @@ import Footer from "./components/Footer";
 import MobileFooter from "./components/MobileFooter";
 
 const App = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <>
       <BrowserRouter>
@@ -33,7 +20,8 @@ const App = () => {
             />
           ))}
         </Routes>
-        {isMobile ? <MobileFooter /> : <Footer />}
+        <Footer />
+        <MobileFooter />
       </BrowserRouter>
     </>
   );
