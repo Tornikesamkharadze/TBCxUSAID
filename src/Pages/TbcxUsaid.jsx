08a9443f07bcf3302d87fdafc31461510fc07236
Tornikesamkharadze <tornikesmakharadzee@gmail.com>
2024-01-23 19:50:16 +0400

    added slider

--- src/Pages/TbcxUsaid.jsx
+++ src/Pages/TbcxUsaid.jsx
@@ -3,12 +3,15 @@ import TbcTeam from "../components/TbcTeam";
 import TbcInfo from "../components/TbcInfo";
 import CoursesCards from "../components/CoursesCards";
 import TbcQnA from "../components/TbcQnA";
+import ProjectPartnerSlider from "../components/ProjectPartnerSlider";
+
 const TbcxUsaid = () => {
   return (
     <>
       <TbcTeam />
       <TbcInfo />
       <CoursesCards />
+      <ProjectPartnerSlider />
       <TbcQnA />
     </>
   );
